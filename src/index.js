271cc5ab7ea4d273a81aeb526cde9a0cdbac0bee
Marlan Perumal <marlan.perumal@gmail.com>
2020-04-05 21:36:51 +0200

    completed tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,118 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import "@atlaskit/css-reset"
+import styled from "styled-components";
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import initialData from "./initial-data"
+import Column from "./Column"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Container = styled.div`
+    display: flex;
+`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ColumnList = React.memo(function ColumnList({ column, tasks, index }) {
+    const colTasks = column.taskIds.map((taskId) => tasks[taskId])
+    return (
+        <Column
+            key={column.id}
+            column={column}
+            tasks={colTasks}
+            index={index}
+        />
+    )
+})
+
+function App() {
+    const [columnOrder, setColumnOrder] = useState(initialData.columnOrder)
+    const [tasks, setTasks] = useState(initialData.tasks)
+    const [columns, setColumns] = useState(initialData.columns)
+    
+    function onDragEnd({ destination, source, draggableId, type }) {
+        if (!destination) {
+            return
+        }
+
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return
+        }
+
+        if (type === "column") {
+            const newColumnOrder = columnOrder.slice()
+            newColumnOrder.splice(source.index, 1)
+            newColumnOrder.splice(destination.index, 0, draggableId)
+            setColumnOrder(newColumnOrder)
+        }
+        else if (type === "task") {
+            
+            const start = columns[source.droppableId]
+            const finish = columns[destination.droppableId]
+    
+            if (start === finish) {
+                const newTaskIds = start.taskIds.slice()
+                newTaskIds.splice(source.index, 1)
+                newTaskIds.splice(destination.index, 0, draggableId)
+                const newColumn = {
+                    ...start,
+                    taskIds: newTaskIds
+                }
+                setColumns({
+                    ...columns,
+                    [newColumn.id]: newColumn
+                })
+            }
+            else {
+                const startTaskIds = start.taskIds.slice()
+                const finishTaskIds = finish.taskIds.slice()
+                startTaskIds.splice(source.index, 1)
+                finishTaskIds.splice(destination.index, 0, draggableId)
+                setColumns({
+                    ...columns,
+                    [start.id]: {
+                        ...start,
+                        taskIds: startTaskIds,
+                    },
+                    [finish.id]: {
+                        ...finish,
+                        taskIds: finishTaskIds
+                    }
+                })
+            }
+        }
+
+    }
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable
+                droppableId="all-columns"
+                direction="horizontal"
+                type="column"
+            >
+                {(provided) => (
+                    <Container
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                    >
+                        { columnOrder.map((columnId, index) => {
+                            const column = columns[columnId]
+                            return (
+                                <ColumnList
+                                    key={column.id}
+                                    column={column}
+                                    tasks={tasks}
+                                    index={index}
+                                />
+                            )
+                        })}
+                        {provided.placeholder}
+                    </Container>
+                )}
+            </Droppable>
+        </DragDropContext>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
